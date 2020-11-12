48a25dcca7ddbd041c5f84a0ef4fb634b012cea0
quangbees <1851171628@e.tlu.edu.vn>
2020-11-12 20:38:29 +0700

    sua header

--- 3.PROJECT/includes/header.php
+++ 3.PROJECT/includes/header.php
@@ -1,12 +1,6 @@
 <!-- Navigation -->
 		<!--nav1-->
-		<style>
-			.menuu ul li a:hover{
-			background: #003478;
-			color: white;
-			}
-			.dropdown:hover .dropdown-menu {display: block;}
-		</style>
+		
 		<div class="aa" style="width: 1080px; margin-left: 16px;">
 			<div class="container-fluid menu-area style">
 				<div class="row">
@@ -18,13 +12,13 @@
 								<div class="row" style="background-color: #003478; margin-left: 0px; border-radius: 0px 0px 10px 50px;  height: 43px; ">
 									<div class="col-md-6">
 										<div>
-											<a  href="includes/Login.php" style="margin-left: 10px;margin-top: 0px;padding-top: 3px;padding-right: 0px;color: white;float: left; ">Đăng nhập</a>
+											<a  href="login.php" style="margin-left: 10px;margin-top: 0px;padding-top: 3px;padding-right: 0px;color: white;float: left; ">Đăng nhập</a>
 										</div>
 									</div>
 									
 									<div class="col-md-6">
 										<div >
-											<a  href="includes/Register.php" style="margin-left: 17px;margin-top: 0px;padding-top: 3px;padding-left: 0px;color: white; ">Đăng ký</a>
+											<a  href="login.php" style="margin-left: 17px;margin-top: 0px;padding-top: 3px;padding-left: 0px;color: white; ">Đăng ký</a>
 										</div>
 									</div>
 								</div>
@@ -36,96 +30,3 @@
 
 		<!--nav2-->
 		
-		<div class="container ame" >	
-			
-			<nav class=" navbar-expand-lg  navbar-dark navbar">   
-
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"><span class="navbar-toggler-icon"></span></button> 
-				<a class="navbar-brand" href="#"></a>
-				<div class="collapse navbar-collapse menuu" id="bs-example-navbar-collapse-1">
-					<ul class="navbar-nav" style="">
-
-						<li class="nav-item ">
-							<a class="nav-link" href="#" style="font-size: 22px;"><i class="fas fa-home"></i> <span class="sr-only"></span></a>
-						</li>
-
-						
-						<li class="nav-item dropdown">
-							<a class="nav-link " id="navbarDropdownMenuLink" data-toggle="dropdown" style="font-size: 15px;margin-top: 5px;">GIỚI THIỆU</a>
-							<div class="dropdown-menu " aria-labelledby="navbarDropdownMenuLink" >
-								<a class="dropdown-item" href="#">Logo Khoa  CNTT</a> 
-								<a class="dropdown-item" href="#">Lời chào mừng</a>
-								<a class="dropdown-item" href="#">Tổ chức</a> 
-								<a class="dropdown-item" href="#">Hợp tác liên kết</a>
-							</div>
-						</li>
-						
-
-						<li class="nav-item dropdown">
-							<a class="nav-link " href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" style="font-size: 15px;margin-top: 5px;">NGHIÊN CỨU KHOA HỌC</a>
-							<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-								<a class="dropdown-item" href="#">Các đề tài dự án</a> 
-								<a class="dropdown-item" href="#">Thông tin seminar</a> 
-								<a class="dropdown-item" href="#">Công trình công bố</a>
-								<a class="dropdown-item" href="#">Các phòng thí nghiệm</a>
-							</div>
-						</li>
-
-
-
-						<li class="nav-item dropdown">
-							<a class="nav-link " href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" style="font-size: 15px;margin-top: 5px;">ĐÀO TẠO</a>
-							<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-								<a class="dropdown-item" href="#">Đào tạo khoa học</a> 
-								<a class="dropdown-item" href="#">Đào tạo sau đại học</a> 
-								<a class="dropdown-item" href="#">Chuẩn đầu ra</a>
-								<a class="dropdown-item" href="#">Định hướng ngành nghề</a>
-								<a class="dropdown-item" href="#">Mô hình đào tạo</a>
-								<a class="dropdown-item" href="#">Đề cương môn học</a>
-							</div>
-						</li>
-
-						<li class="nav-item dropdown">
-							<a class="nav-link " href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" style="font-size: 15px;margin-top: 5px;">BỘ MÔN - TRUNG TÂM</a>
-							<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-								<a class="dropdown-item" href="#">CN Phần mềm</a> 
-								<a class="dropdown-item" href="#">Hệ thống thông tin</a> 
-								<a class="dropdown-item" href="#">Khoa học máy tính và mạng</a>
-								<a class="dropdown-item" href="#">Toán học</a>
-								<a class="dropdown-item" href="#">Tin học và kĩ thuật tính toán</a>
-								<a class="dropdown-item" href="#">Trung tâm tin học</a>
-							</div>
-
-							<li class="nav-item dropdown">
-								<a class="nav-link " href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" style="font-size: 15px;margin-top: 5px;">TUYỂN SINH</a>
-								
-
-								<li class="nav-item dropdown">
-									<a class="nav-link " href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" style="font-size: 15px;margin-top: 5px;">TIN TỨC</a>
-									<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-										<a class="dropdown-item" href="#">Sự kiện</a> 
-										<a class="dropdown-item" href="#">CSE trên báo</a>
-									</div>
-
-									<li class="nav-item dropdown">
-										<a class="nav-link " href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" style="font-size: 15px;margin-top: 5px;">THÔNG BÁO</a>
-										<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-											<a class="dropdown-item" href="#">Thông báo</a> 
-											<a class="dropdown-item" href="#">TB đào tạo</a> 
-											<a class="dropdown-item" href="#">Nghiên cứu khoa học</a>
-											<a class="dropdown-item" href="#">Tuyển dụng</a>
-											<a class="dropdown-item" href="#">Học bổng</a>
-											<a class="dropdown-item" href="#">Thông báo khác</a>
-										</div>
-
-										<li class="nav-item">
-											<a class="nav-link" href="#" style="font-size: 15px;margin-top: 5px;">LIÊN HỆ</a>
-										</li>
-
-
-									</ul>
-								</div>
-							</nav>
-						</div>
-		
-						</div>
